71d00e53442a9a808c2b01a75e45f55229ef6d8f
Hugo BEAUJOUR <hbeaujou@e1r3p5.42.fr>
2015-12-30 18:48:41 +0100

    la norme avance OK -- 2

--- ft_printf/libft.h
+++ ft_printf/libft.h
@@ -6,7 +6,7 @@
 /*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 15:08:57 by ebouther          #+#    #+#             */
-/*   Updated: 2015/12/30 14:26:38 by hbeaujou         ###   ########.fr       */
+/*   Updated: 2015/12/30 18:41:44 by hbeaujou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,8 @@ char				*ft_strrchr(const char *s, int c);
 char				*ft_strnstr(const char *haystack, const char *needle,
 		size_t n);
 char				*ft_strstr(const char *haystack, const char *needle);
-void				ft_ulltstr_base(unsigned long long n, char *base_str, char *buf);
+void				ft_ulltstr_base(unsigned long long n,
+		char *base_str, char *buf);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 char				*ft_ntoa_base(long long n, char *base);
