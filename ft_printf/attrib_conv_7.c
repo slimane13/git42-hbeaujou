8fcff1140663d536a5ad1d0c2cde043a1f5b9ca0
Hugo BEAUJOUR <hbeaujou@e1r3p5.42.fr>
2015-12-30 17:15:01 +0100

    7 FINI 5/5 OK

--- ft_printf/attrib_conv_7.c
+++ ft_printf/attrib_conv_7.c
@@ -6,7 +6,7 @@
 /*   By: hbeaujou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/21 14:17:49 by hbeaujou          #+#    #+#             */
-/*   Updated: 2015/12/30 17:07:28 by hbeaujou         ###   ########.fr       */
+/*   Updated: 2015/12/30 17:14:34 by hbeaujou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,82 +105,22 @@ void	attrib_u_z(char **str, t_var **var, int count[3])
 
 void	attrib_o_z(char **str, t_var **var, int count[3])
 {
-	int nbr;
-	int flag;
-	int k;
-	int l;
-	char c;
+	t_cut t_v;
 
-	k = 0;
-	c = 't';
-	nbr = ft_nbrlen(var[count[2]]->u_l_long);
-	flag = check_flag(str, count, &nbr, &c);
-	l = ft_atoi_spec_o(str[count[0]]);
-	str[count[0]] = ft_ntoa_base_un(var[count[2]]->u_l_long, "01234567");
-	nbr = ft_strlen(str[count[0]]);
-	if (flag == 1000)
+	ft_cut_init_o_z(&t_v, var, str, count);
+	if (t_v.flag == 1000)
 		str[count[0]] = ft_strjoin("+", str[count[0]]);
-	else if (flag == 2000)
+	else if (t_v.flag == 2000)
 		str[count[0]] = ft_strjoin(" ", str[count[0]]);
-	else if (flag == 5000)
+	else if (t_v.flag == 5000)
 	{
 		str[count[0]] = ft_strjoin("0", str[count[0]]);
-		flag = l;
+		t_v.flag = t_v.l;
 	}
-	else if (flag == 3000)
-	{
-		if (var[count[2]]->stars < 0)
-		{
-			while (k > var[count[2]]->stars + nbr - 1)
-			{
-				str[count[0]] = ft_strjoin(str[count[0]], " ");
-				k--;
-			}
-		}
-		else
-		{
-			while (k < var[count[2]]->stars - nbr + 1)
-			{
-				str[count[0]] = ft_strjoin(" ", str[count[0]]);
-				k++;
-			}
-		}
-	}
-	if (flag != -1 && flag != 1000 &&
-			flag != 2000 && flag != 3000 && flag != 5000)
-	{
-		if (flag < -1 && c != '0')
-		{
-			while (k > flag + nbr)
-			{
-				str[count[0]] = ft_strjoin(str[count[0]], " ");
-				k--;
-			}
-		}
-		else if (flag > 0 && (c == '0' || c == '.'))
-		{
-			while (k < flag - nbr)
-			{
-				str[count[0]] = ft_strjoin("0", str[count[0]]);
-				k++;
-			}
-		}
-		else if (flag < -1 && c == '0')
-		{
-			while (k > flag)
-			{
-				str[count[0]] = ft_strjoin("0", str[count[0]]);
-				k--;
-			}
-		}
-		else
-		{
-			while (k < flag - nbr)
-			{
-				str[count[0]] = ft_strjoin(" ", str[count[0]]);
-				k++;
-			}
-		}
-	}
+	else if (t_v.flag == 3000)
+		ft_cut_flag_o1_z(&t_v, var, str, count);
+	if (t_v.flag != -1 && t_v.flag != 1000 &&
+			t_v.flag != 2000 && t_v.flag != 3000 && t_v.flag != 5000)
+		ft_cut_flag_o2_z(&t_v, str, count);
 	count[2]++;
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hbeaujou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 15:28:05 by hbeaujou          #+#    #+#             */
-/*   Updated: 2015/12/30 17:01:40 by hbeaujou         ###   ########.fr       */
+/*   Updated: 2015/12/30 17:09:07 by hbeaujou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,12 @@ int		run_var(char *str, char c);
 int		ft_wtomb(char *s, wchar_t wchar);
 
 int		ft_printf(char *format, ...);
+
+void	ft_cut_init_o_z(t_cut *t_v, t_var **var, char **str, int count[3]);
+void    ft_cut_flag_o1_z(t_cut *t_v, t_var **var, char **str, int count[3]);
+void    ft_cut_flag_o2_z(t_cut *t_v, char **str, int count[3]);
+void    ft_cut_flag_o2_1z(t_cut *t_v, char **str, int count[3]);
+void    ft_cut_flag_o2_2z(t_cut *t_v, char **str, int count[3]);
 
 void	ft_cut_init_u_z(t_cut *t_v, t_var **var, char **str, int count[3]);
 void    ft_cut_flag_u1_z(t_cut *t_v, char **str, int count[3]);
