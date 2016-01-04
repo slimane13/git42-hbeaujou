a3f09e3ddb1a002f08eee8f0b3e478c02ff61542
Hugo BEAUJOUR <hbeaujou@e2r4p4.42.fr>
2016-01-04 12:56:19 +0100

    c partir pour le lem in

--- lem_in/ft_check_file.c
+++ lem_in/ft_check_file.c
@@ -1,24 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lem_in.h                                           :+:      :+:    :+:   */
+/*   ft_check_file.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbeaujou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/03 14:20:35 by hbeaujou          #+#    #+#             */
-/*   Updated: 2016/01/04 12:47:23 by hbeaujou         ###   ########.fr       */
+/*   Created: 2016/01/04 12:48:45 by hbeaujou          #+#    #+#             */
+/*   Updated: 2016/01/04 12:49:01 by hbeaujou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LEM_IN_H
-# define LEM_IN_H
+#include "lem_in.h"
 
-# include "./ft_printf/ft_printf.h"
 
-typedef struct	s_room
-{
-	char		*name;
-	int			ant;
-	t_room		*t_nexts;
-	t_room		*t_previous;
-}				t_room;
